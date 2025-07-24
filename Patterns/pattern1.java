fa0ddc67c6b0117539456f8dbbc705f967112d91
2003chinmayee <chinmayeepatil2003@gmail.com>
2025-07-24 11:14:50 +0530

    adding 1st pattern question

---
+++ Patterns/pattern1.java
@@ -0,0 +1,16 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        int n=4;
+        int m=5;
+        for(int i = 1; i <= n; i++) {  // here we created 2 loops called nested loops i for vertical 
+            for (int j = 1;  j<= m; j++) {//and j for horizantal
+                System.out.print("*");//we write print instead of println because we want horizantal * in one line
+                
+            }
+            System.out.println();//this is written to go to next 
+            
+        }}
+        
+    }
+
+
